a7fb180b569123de4a0c0c0afb0c2ce3f80512bb
Daniil Bodryagin <daniilMAOmail@gmail.com>
2022-09-11 10:40:02 +0300

    JS-DOM task first commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const allContent = [
+  {"Intro": ""},
+  {"The Orion Nebula": ""},
+  {"The Praesepe Cluster": ""},
+  {"The Andromeda Galaxy": ""},
+  {"The Hercules Cluster": ""},
+  {"The Ring Nebula": ""}
+]
